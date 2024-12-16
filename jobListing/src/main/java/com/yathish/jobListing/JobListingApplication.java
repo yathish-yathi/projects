0c66e88347f3f5aa4b3b67576c86e6a13c4d37f3
yathish-yathi <140341558+yathish-yathi@users.noreply.github.com>
2024-12-16 11:25:11 +0530

    Update JobListingApplication.java

--- jobListing/src/main/java/com/yathish/jobListing/JobListingApplication.java
+++ jobListing/src/main/java/com/yathish/jobListing/JobListingApplication.java
@@ -10,24 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class JobListingApplication {
 
-//	@Bean
-//	public Docket api() {
-//		return new Docket(DocumentationType.SWAGGER_2)
-//				.select()
-//				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)) // Adjust package
-//				.paths(PathSelectors.any())
-//				.build().apiInfo(apiInfo()).useDefaultResponseMessages(false);
-//	}
-//
-//	@Bean
-//	public ApiInfo apiInfo() {
-//		final ApiInfoBuilder builder = new ApiInfoBuilder();
-//		return builder.build();
-//	}
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(JobListingApplication.class, args);
 	}
-
 }
